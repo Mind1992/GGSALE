215b943749f36382d837146d2175b64c7d8bc912
Sergii Mertsalov <sergmerts@gmail.com>
2014-10-16 08:09:42 -0700

    clean up indentation in controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,30 @@
 class UsersController < ApplicationController
-	def index
-	  @users = User.all
-		@sale = @users.sales
-	end
+  def index
+    @users = User.all
+    @sale = @users.sales
+  end
 
-	def new
-		@user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
-	def create
-		@user = User.new(user_params)
-		if @user.save
-			redirect_to root_path
-		else
-			'new'
-		end
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      'new'
+    end
+  end
 
-	def show
-		@user = current_user
-		@sales = @user.sales
-	end
+  def show
+    @user = current_user
+    @sales = @user.sales
+  end
 
 private
 
-	def user_params
-		params.require(:user).permit(:email, :password, :password_confirmation, :remember_me, :username)
-	end
-end
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation, :remember_me, :username)
+    end
+  end
